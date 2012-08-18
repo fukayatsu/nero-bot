9b119bb4df6b33c2f60d6720dede09d6da95152e
fukayatsu <fukayatsu@gmail.com>
2012-08-18 17:27:58 +0900

    Add valid_api_list

--- nero-bot.rb
+++ nero-bot.rb
@@ -22,15 +22,16 @@ require 'mongo'
 SETTING_FILE = File.join(File.expand_path(File.dirname(__FILE__)), '.env')
 
 class NeroBot
-  def initialize(setting_file = SETTING_FILE)
+  def initialize setting_file = SETTING_FILE
     settings = load_settings setting_file
 
     configure_twitter(settings)
     configure_db
   end
 
-  def fetch_statuses(api_name)
-    # TODO api_nameのホワイトリスト
+  def fetch_statuses api_name
+    valid_api_names = [:mentions, :home_timeline]
+    raise "invalid api name: #{api_name}" unless valid_api_names.include? api_name.to_sym
     api_name = api_name.to_s
 
     options = {
@@ -41,7 +42,7 @@ class NeroBot
     statuses = Twitter.method(api_name).call(options)
       .map{ |tw_status|
         status = tw_status.to_hash
-        {id: status[:id_str], data: status}
+        { id: status[:id_str], data: status }
       }
 
     return 0 if statuses.size == 0
@@ -50,9 +51,60 @@ class NeroBot
     since_id_of(api_name, statuses.first[:id])
   end
 
+  def process_mentions
+    tasks = @db['mentions'].find({state: {'$ne' => 'done'}})
+      .map{ |mention|
+        text = mention['data']['text']
+        mt = text.match(/^@yoiko_ha_nero ([a-z0-9-]*)$/)
+        next unless mt && mt.size == 2
+
+        message = mt[1]
+        m_user = mention['data']['user']
+        user = {id: m_user['id_str'], screen_name: m_user['screen_name']}
+
+        { id: mention['id'], message: message, user: user }
+      }.compact
+
+    excecute_tasks tasks
+  end
+
+  def excecute_tasks valid_tasks
+    valid_tasks.each do |task|
+      case task[:message]
+      when /^stop$/
+        # その日はそれ以上監視しない
+
+      when /^start$/
+        # 監視の一時停止を解除、もしくはデフォルト設定で開始(00:00-6:00)
+
+      when /^remove$/
+        # アンフォローして監視をやめる
+
+      when /^([0-9]{2})([0-9]{2})-([0-9]{2})([0-9]{2})$/
+        # ユーザーをフォローして、監視時間を設定
+        # p $1, $2, $3, $4
+      else
+        # なにもしないでmension:sate=>done
+        mention_state_of task[:id], :done
+      end
+    end
+  end
+
+  def mention_state_of mention_id, update_state = nil
+    mentions = @db['mentions']
+    condition = {id: mention_id}
+
+    if update_state
+      doc = { state: update_state.to_s }
+      insert_or_update(mentions, condition, doc)
+    else
+      mentions.find_one(condition)['state']
+    end
+  end
+
 private
 
-  def configure_twitter(settings)
+  def configure_twitter settings
     [
       'consumer_key',
       'consumer_secret',
@@ -67,7 +119,7 @@ private
     @db = Mongo::Connection.new.db('nero_bot');
   end
 
-  def load_settings(setting_file)
+  def load_settings setting_file
     list = open(setting_file)
       .each
       .map { |line|
@@ -81,7 +133,7 @@ private
     Hash[*list]
   end
 
-  def since_id_of(name, update_id = nil)
+  def since_id_of name, update_id = nil
     bot_info = @db['bot_info']
     condition = {name: name}
 
@@ -91,26 +143,24 @@ private
       insert_or_update(bot_info, condition, doc)
     else
       # 取得
-      info = bot_info.find_one condition
-      info ?
-        info['since_id'] : 1
+      bot_info.find_one(condition)['since_id'] || 1
     end
   end
 
-  def insert_or_update(collection, condition, doc)
+  def insert_or_update collection, condition, doc
     if collection.find_one condition
       # 更新
-      collection.update(condition, {'$set' => doc})
+      collection.update(condition, { '$set' => doc })
     else
       # 新規作成
       collection.insert doc.merge(condition)
     end
   end
 
-  def insert_each_when_absent(collection, docs)
+  def insert_each_when_absent collection, docs
     docs.each do |doc|
       # すでに同じidのdocが存在するならスキップ
-      next if collection.find_one({id: doc[:id]})
+      next if collection.find_one({ id: doc[:id] })
       collection.insert(doc)
     end
   end
@@ -122,8 +172,12 @@ if __FILE__ == $PROGRAM_NAME
   #TODO テスト
 
   bot = NeroBot.new
-  bot.fetch_statuses :mentions
-  bot.fetch_statuses :home_timeline
+  #bot.fetch_statuses :mentions
+  bot.process_mentions
+
+  #bot.fetch_statuses :home_timeline
+  ##bot.process_home_timeline
+
 
 end
 
