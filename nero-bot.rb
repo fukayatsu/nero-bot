61583d8b65b8a13da66d5f5d6ab03849b7c45fd1
fukayatsu <fukayatsu@gmail.com>
2012-08-19 23:49:26 +0900

    Refactor constant

--- nero-bot.rb
+++ nero-bot.rb
@@ -8,7 +8,7 @@ require 'json'
 require 'twitter'
 require 'mongo'
 
-Encoding.default_external="UTF-8"
+Encoding.default_external = Encoding::UTF_8
 
 BASE_PATH = File.expand_path(File.dirname(__FILE__))
 SETTING_FILE = File.join(BASE_PATH, '.env')
